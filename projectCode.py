528c13d465e61a0869bba2585f639e0b0895a44a
saijal <saijal1295@gmail.com>
2018-02-19 02:11:09 +0530

    Skeleton Code
    This is the base code to calculate beta function and the romer integral. Real dataset needs to be supplied. Wait till alpha is determind.

---
+++ projectCode.py
@@ -0,0 +1,31 @@
+#from scipy.stats import beta
+from scipy.integrate import quad
+
+import math
+
+a = 200
+b = 0.80
+ 
+def beta_1(a,b):
+     
+    '''uses gamma function or inbuilt math.gamma() to compute values of beta function'''
+     
+    #beta = math.gamma(a)*math.gamma(b)/math.gamma(a+b)
+    beta = math.exp(math.lgamma(a) + math.lgamma(b) - math.lgamma(a+b))
+    return beta
+
+
+solution = beta_1(a,b);
+print "beta_value = ",solution
+
+
+#from scipy.integrate import quad
+
+def integrand(solution):
+    return solution
+
+ans, err = quad(integrand, 0, 20)
+print "integration value = ",ans
+
+
+
