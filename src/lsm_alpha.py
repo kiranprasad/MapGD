c1f1684755964e110d1eb727ef18c84885d8b3a9
Kiran Prasad <kiran.prasad@hotmail.com>
2018-02-25 16:54:31 +0530

    placeholder commit Reshuffling files between branches

--- src/lsm_alpha.py
+++ src/lsm_alpha.py
@@ -2,7 +2,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import beta, gamma
+from scipy.special import beta
 
 '''
 plot shtuff using basic plotting
@@ -17,21 +17,20 @@ def plot_things (xaxis, yaxis):
 def csvread (filename, ftype):
     
     with open(filename, newline='') as csvfile:
-        csvarray=np.loadtxt(csvfile, dtype=ftype, skiprows=1, delimiter=',')
+        csvarray=np.loadtxt(csvfile, dtype=ftype, skiprows=0, delimiter=',')
 
     return csvarray
-    '''
-    csv list to ndarray conversions. 
-    If there is better solution, please fix and comment
-    '''
 
-csvx=[]
-csvy=[]
-csvint = csvread('/home/kranep/MapGD/Project-Data/csv-files/ICARUS.csv','float') #.astype(float)     
-for x in range(0,np.ma.size(csvint,0)):
-    csvx.append([csvint[x][0]])
-    csvy.append([csvint[x][1]])
-xn = np.array(csvx)
-yn = np.array(csvy)
-print(xn)
+'''
+Flatfiles for all input parameters.
+'''
+sjr = csvread('filename.csv','int') #Y
+betainput = csvread('betafunc.csv','float') #i and p
+totalarticles = csvread('articlecount.csv', 'int') #A
+
+'''
+setting PSI to 1 and evaluating alpha from Romer model using stochastic model of citation
+''' 
+alpha = []
+alpha.append(np.log(Y) - A*np.log(beta(i,p+1))) / (np.log(T)-A*np.log(beta(i,p+1))))
 
